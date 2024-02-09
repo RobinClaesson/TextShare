f7b6e15044f304d5ac39bccff9cad4b891cf855e
Robin Claesson <robcla@kth.se>
2024-02-09 21:29:45 +0100

    Changed project template to MVC
    This is to easier be able to show all stored texts in a web interface

---
+++ TextShare/TextShareServer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TextShareServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
