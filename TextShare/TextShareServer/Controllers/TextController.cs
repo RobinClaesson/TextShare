7c0a09c04ce7ed9d1afdb3c808b00bab70f063e8
Robin Claesson <robcla@kth.se>
2024-02-09 18:46:35 +0100

    Base API with store and read features

---
+++ TextShare/TextShareServer/Controllers/TextController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TextShareServer.Services;
+
+namespace TextShareServer.Controllers;
+
+[Route("api/")]
+[ApiController]
+public class TextController : ControllerBase
+{
+    private FileService _fileService;
+
+    public TextController(FileService fileService)
+    {
+        _fileService = fileService;
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(string id)
+    {
+        try
+        {
+            var text = _fileService.GetText(id);
+
+            if (text == string.Empty)
+                return NotFound();
+
+            return Ok(text);
+        }
+        catch
+        {
+            return new ObjectResult(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpPost("{id}/{value}")]
+    public IActionResult Post(string id, string value)
+    {
+        try
+        {
+            _fileService.StoreText(id, value);
+        }
+        catch
+        {
+            return new ObjectResult(StatusCodes.Status500InternalServerError);
+        }
+
+        return Ok();
+    }
+}
---
+++ TextShare/TextShareServer/Services/FileService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authentication.OAuth.Claims;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace TextShareServer.Services;
+
+public class FileService
+{
+    public const string TextStorageFolder = "StoredTexts";
+
+    public FileService()
+    {
+        if (!Directory.Exists(TextStorageFolder))
+        {
+            Directory.CreateDirectory(TextStorageFolder);
+        }
+    }
+
+    public string GetTextFilePath(string id)
+    {
+        return Path.Combine(TextStorageFolder, id + ".txt");
+    }
+
+    public string GetText(string id)
+    {
+        var filePath = GetTextFilePath(id);
+        if (!File.Exists(filePath))
+        {
+            return string.Empty;
+        }
+        return File.ReadAllText(filePath);
+    }
+
+    public void StoreText(string id, string text)
+    {
+        var filePath = GetTextFilePath(id);
+        using(var writer = new StreamWriter(filePath, true))
+        {
+            writer.WriteLine(text);
+        }
+    }
+
+    public void DeleteText(string id)
+    {
+        var filePath = GetTextFilePath(id);
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+        }
+    }
+}
